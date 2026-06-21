9252c22d142d4e63e7224739370c58f41a474202
Rishin M <172200911+Lindalu0531@users.noreply.github.com>
2026-06-21 18:07:34 +0530

    Added README.md, fixed some bugs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Pass-Gen
 A Python Password Genarator 
+A simple Python program that generates long random string that conatains alphabets, numbers,
+and special charectors. 
+I dont think this will the final version
--- main.py
+++ main.py
@@ -15,7 +15,8 @@ print("Welcome to password Generator")
 try:
     length = int(input("Enter the amount of charectors u want(max 128): "))
     if length > 128 or  length < 0:
-        print(pass_gen(length))
+        print("Error: Please enter between 0 and 128")
+        pass_gen(length)
     else:
         print(f"Your generatod password is: {pass_gen(length)}")
 except ValueError:
